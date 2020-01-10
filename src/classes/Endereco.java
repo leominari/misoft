b0a7941f3651c4f9b45f411996676a6611e1d8bb
Leonardo Minari <minari.leo@gmail.com>
2020-01-10 09:08:55 -0300

    v0.2

--- src/classes/Endereco.java
+++ src/classes/Endereco.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package misoft.classes;
+package classes;
 
-import misoft.api.Cep;
+import api.Cep;
 
 /**
  *
