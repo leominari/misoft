8352091d54bc8b2914574db699ace667c78f6f46
Leonardo Minari <minari.leo@gmail.com>
2020-01-27 16:28:30 -0300

    v0.48

--- src/view/VAlterarPedido.java
+++ src/view/VAlterarPedido.java
@@ -32,11 +32,11 @@ public class VAlterarPedido extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 583, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 192, Short.MAX_VALUE)
         );
 
         pack();
